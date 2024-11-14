da6133ed05513e740f3da867aa1bdc90ca25f102
wcarter <will.carter@lrewater.com>
2024-11-13 19:51:29 -0700

    rework star 7 pt

---
+++ test/update_custom_icon_properties.sql
@@ -0,0 +1,87 @@
+UPDATE data.list_map_sym SET shape = 'triangle', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 1;
+UPDATE data.list_map_sym SET shape = 'circle', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 2;
+UPDATE data.list_map_sym SET shape = 'square', fill_color = 'green', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 3;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 3, stroke_color = 'black' WHERE map_sym_ndx = 4;
+UPDATE data.list_map_sym SET shape = 'triangle', fill_color = 'orange', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 5;
+UPDATE data.list_map_sym SET shape = 'circle', fill_color = 'purple', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 6;
+UPDATE data.list_map_sym SET shape = 'square', fill_color = 'pink', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 7;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'brown', stroke_width = 3, stroke_color = 'black' WHERE map_sym_ndx = 8;
+UPDATE data.list_map_sym SET shape = 'triangle', fill_color = 'teal', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 9;
+UPDATE data.list_map_sym SET shape = 'circle', fill_color = 'gray', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 10;
+UPDATE data.list_map_sym SET shape = 'square', fill_color = 'olive', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 11;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'white', stroke_width = 3, stroke_color = 'black' WHERE map_sym_ndx = 12;
+UPDATE data.list_map_sym SET shape = 'triangle', fill_color = 'cyan', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 13;
+UPDATE data.list_map_sym SET shape = 'circle', fill_color = 'magenta', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 14;
+UPDATE data.list_map_sym SET shape = 'square', fill_color = 'lime', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 15;
+
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 1;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 2;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 3;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 4;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 5;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 6;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 7;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 8;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 9;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 10;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 11;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 12;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 13;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 14;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'red', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 15;
+
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 1;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 2;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 3;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 4;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 5;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 6;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 7;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 8;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 9;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 10;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 11;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 12;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 13;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 14;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'blue', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 15;
+
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 1;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 2;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 3;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 4;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 5;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 6;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 7;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 8;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 9;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 10;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 11;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 12;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 13;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 14;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'yellow', stroke_width = 1, stroke_color = 'black' WHERE map_sym_ndx = 15;
+
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 1;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 2;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 3;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 4;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 5;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 6;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 7;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 8;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 9;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red'WHERE map_sym_ndx = 10;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 11;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 12;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 13;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 14;
+UPDATE data.list_map_sym SET shape = 'star-7pt', fill_color = 'green', stroke_width = 5, stroke_color = 'red' WHERE map_sym_ndx = 15;
+
+select * from ui.list_custom_sprites;
+--This view defines the custom icons that will be added to the custom sprite
+
+select from ui.refresh_sprites();
+--Queries data.list_map_sym to see what new custom icons to create in the sprite
+--Builds sprite for the 5 basemaps: dark, light, outdoors, satellite, streets. 2 sprites are created for each basemap. 20 files are generated.
+--New sprite images and json are uploaded to s3 bucket
